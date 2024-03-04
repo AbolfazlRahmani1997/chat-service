747a4fb496a2b9b37c307268a6e249cac3e9a4e2
abolfazl <abolfazlrahmani1997@gmail.com>
2024-03-04 11:51:03 +0330

    Fix Rabit Mq

--- cmd/main.go
+++ cmd/main.go
@@ -5,12 +5,15 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"golang.org/x/net/context"
+	"os"
 	"server/internal/ws"
 	"server/router"
 )
 
 func main() {
-	clientOptions := options.Client().ApplyURI("mongodb://127.0.0.1:27017")
+
+	mongoUrl := fmt.Sprintf("mongodb://%s:27017", os.Getenv("MongoDbUrl"))
+	clientOptions := options.Client().ApplyURI(mongoUrl)
 	// Connect to MongoDB
 	client, _ := mongo.Connect(context.TODO(), clientOptions)
 	hub := ws.NewHub(client)
