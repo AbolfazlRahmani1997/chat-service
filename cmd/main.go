b60fe50871872ae8df4f44fae0de586776831177
abolfazl <abolfazlrahmani1997@gmail.com>
2024-03-05 15:08:23 +0330

    fix rabbit

--- cmd/main.go
+++ cmd/main.go
@@ -5,20 +5,21 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"golang.org/x/net/context"
+	"os"
 	"server/internal/ws"
 	"server/router"
 )
 
 func main() {
 
-	mongoUrl := fmt.Sprintf("mongodb://127.0.0.1:27017")
-	//credential := options.Credential{
-	//	Username: "",
-	//	Password: "",
-	//}
-	option := options.ServerAPI(options.ServerAPIVersion1)
-	clientOptions := options.Client().ApplyURI(mongoUrl).SetServerAPIOptions(option)
-	client, _ := mongo.Connect(context.Background(), clientOptions)
+	mongoUrl := fmt.Sprintf("mongodb://%s:27017", os.Getenv("MONGO_DB_HOST"))
+	credential := options.Credential{
+		Username: os.Getenv("MONGO_DB_USERNAME"),
+		Password: os.Getenv("MONGO_DB_PASSWORD"),
+	}
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	clientOptions := options.Client().ApplyURI(mongoUrl).SetAuth(credential).SetServerAPIOptions(serverAPI)
+	client, _ := mongo.Connect(context.TODO(), clientOptions)
 	hub := ws.NewHub(client)
 	wsHandler := ws.NewHandler(hub)
 	go hub.Run()
