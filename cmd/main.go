97bd0fb10ebaa6cfb6f2120b2446840dc8a205ee
abolfazlrahmani1997 <abolfazlrahmani1997@gmail.com>
2024-03-14 13:37:43 +0330

    add user repository to service

--- cmd/main.go
+++ cmd/main.go
@@ -5,20 +5,21 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"golang.org/x/net/context"
+	"os"
 	"server/internal/ws"
 	"server/router"
 )
 
 func main() {
 
-	mongoUrl := fmt.Sprintf("mongodb://%s:%s/", "127.0.0.1", "27017")
-	//credential := options.Credential{
-	//	Username: os.Getenv("MONGO_DB_USERNAME"),
-	//	Password: os.Getenv("MONGO_DB_PASSWORD"),
-	//}
+	mongoUrl := fmt.Sprintf("mongodb://%s:%s/", os.Getenv("MONGO_DB_USERNAME"), os.Getenv("MONGO_DB_PASSWORD"))
+	credential := options.Credential{
+		Username: os.Getenv("MONGO_DB_USERNAME"),
+		Password: os.Getenv("MONGO_DB_PASSWORD"),
+	}
 
 	//serverAPI := options.ServerAPI(options.ServerAPIVersion1)
-	clientOptions := options.Client().ApplyURI(mongoUrl)
+	clientOptions := options.Client().ApplyURI(mongoUrl).SetAuth(credential)
 	client, err := mongo.Connect(context.TODO(), clientOptions)
 	if err != nil {
 		fmt.Println(err)
