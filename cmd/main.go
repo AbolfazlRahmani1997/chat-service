56a6e04e4af78377a146e1a6ab20340d3f23ce75
abolfazlrahmani1997 <abolfazlrahmani1997@gmail.com>
2024-04-04 08:46:07 +0330

    fix online and offline sync room and evade to status and not working

--- cmd/main.go
+++ cmd/main.go
@@ -5,20 +5,21 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"golang.org/x/net/context"
+	"os"
 	"server/internal/ws"
 	"server/router"
 )
 
 func main() {
 
-	mongoUrl := fmt.Sprintf("mongodb://%s:%s/", "127.0.0.1", "27017")
-	//credential := options.Credential{
-	//	Username: "root",
-	//	Password: "root",
-	//}
+	mongoUrl := fmt.Sprintf("mongodb://%s:%s/", os.Getenv("MONGO_DB_HOST"), os.Getenv("MONGO_DB_PORT"))
+	credential := options.Credential{
+		Username: os.Getenv("MONGO_DB_USERNAME"),
+		Password: os.Getenv("MONGO_DB_PASSWORD"),
+	}
 
 	//serverAPI := options.ServerAPI(options.ServerAPIVersion1)
-	clientOptions := options.Client().ApplyURI(mongoUrl)
+	clientOptions := options.Client().ApplyURI(mongoUrl).SetAuth(credential)
 	client, err := mongo.Connect(context.TODO(), clientOptions)
 	if err != nil {
 		fmt.Println(err)
@@ -30,7 +31,7 @@ func main() {
 	go hub.Run()
 	go hub.Manager()
 	router.InitRouter(wsHandler)
-	err = router.Start("0.0.0.0:8088")
+	err = router.Start("0.0.0.0:8080")
 	if err != nil {
 		fmt.Println(err)
 		return
